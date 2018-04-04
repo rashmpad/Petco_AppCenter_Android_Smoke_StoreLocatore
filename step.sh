bfc1355926eb3b99d448bc1a6382d1717524cce7
rashmpad <37831236+rashmpad@users.noreply.github.com>
2018-04-04 11:30:21 +0530

    Update step.sh

--- step.sh
+++ step.sh
@@ -53,4 +53,4 @@ appcenter test run uitest --app "${app_center_app}" --devices 6f2c8184 --app-pat
 # --- Exit codes:
 # The exit code of your Step is very important. If you return
 #  with a 0 exit code `bitrise` will register your Step as "successful".
-# Any non zero exit code will be registered as "failed" by "bitrise".
+# Any non zero exit code will be registered as "failed" by bitrise.
