8429410d66d0f315e3f3a590952a478b6cc185be
rashmpad <37831236+rashmpad@users.noreply.github.com>
2018-04-10 20:54:57 +0530

    Update step.sh

--- step.sh
+++ step.sh
@@ -49,7 +49,6 @@ npm install appcenter-cli@1.0.8 -g
 #nuget restore -NonInteractive "${SOLUTION}"
 msbuild "Petco.UITests/Petco.UITests.csproj" /p:Configuration=Release
 #appcenter test run uitest --app "${app_center_app}" --devices 6f2c8184 --app-path "${app_path}" --async --fixture Petco.UITests.Cart\(Android\).VerifyCartFlowSecureCheckOutForgotPasswordwithOutRepeatDelivery --test-series "master" --locale "en_US" --token "${app_center_token}" --build-dir "Petco.UITests/bin/Release"
-echo "Rashmi + ${app_path}"
 #npm install appcenter-cli@1.0.8 -g
 #appcenter test generate uitest --platform android --output-path "${OUTPUT_PATH}"
 #nuget restore -NonInteractive "${SOLUTION}"
