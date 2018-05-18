40c266fc859b0c519dd004a912035cff8377f2d2
rashmpad <37831236+rashmpad@users.noreply.github.com>
2018-05-18 19:13:00 +0530

    Update step.sh

--- step.sh
+++ step.sh
@@ -31,28 +31,14 @@ echo " (i) Provided app center app: ${app_center_app}"
 echo " (i) Provided app center token: 93424d4f2ed8ba4ba8492a7c7299366eee09a2a9"
 echo "${ANDROID_PROJECT_FOLDER}"
 
-# ---------------------
-# ---------------------
-# --- Main
-#LAUNCH_TEST_DIR="${BITRISE_SOURCE_DIR}/app-center-launch-test-android"
-#OUTPUT_PATH="${LAUNCH_TEST_DIR}/GeneratedTest"
-#SOLUTION="${OUTPUT_PATH}/AppCenter.UITest.Android.sln"
 
 
 ARTIFACTS_DIR="Artifacts"
 BUILD_DIR="Petco.UITests/bin/Release"
 MANIFEST_PATH="${ARTIFACTS_DIR}/manifest.json"
-
-
-#SOLUTION="Petco.UITests.sln"
 npm install appcenter-cli@1.0.8 -g
-#nuget restore -NonInteractive "${SOLUTION}"
 msbuild "Petco.UITests/Petco.UITests.csproj" /p:Configuration=Release
-#appcenter test run uitest --app "${app_center_app}" --devices 6f2c8184 --app-path "${app_path}" --async --fixture Petco.UITests.Cart\(Android\).VerifyCartFlowSecureCheckOutForgotPasswordwithOutRepeatDelivery --test-series "master" --locale "en_US" --token "${app_center_token}" --build-dir "Petco.UITests/bin/Release"
-#npm install appcenter-cli@1.0.8 -g
-#appcenter test generate uitest --platform android --output-path "${OUTPUT_PATH}"
-#nuget restore -NonInteractive "${SOLUTION}"
-#msbuild "${SOLUTION}" /p:Configuration=Releaseappcenter test prepare uitest --artifacts-dir "${ARTIFACTS_DIR}" --app-path "${app_path}" --build-dir "${BUILD_DIR}" --fixture "Petco.UITests.SmokeTest(Android)" --fixture "Petco.UITests.Cart(Android)" --debug --quiet
+
 
 appcenter test prepare uitest --artifacts-dir "${ARTIFACTS_DIR}" --app-path "${app_path}" --build-dir "${BUILD_DIR}" --fixture "Petco.UITests.SmokeTest(Android)" --fixture "Petco.UITests.TheStoreLocator(Android)" --debug --quiet
 
